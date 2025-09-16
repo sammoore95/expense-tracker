4bb6c83796e59e8a3a2afe56095128ee95c61450
Sam Moore <samhmoore95@gmail.com>
2025-09-16 07:45:21 -0400

    feat: add validate_category_selection function to validate user input for expense categories

--- expense-tracker.py
+++ expense-tracker.py
@@ -2,7 +2,7 @@ import json
 from decimal import Decimal, InvalidOperation
 
 def check_for_decimal(promt):
-    """Validates user input is a decimal"""
+    """Validates user input is a decimal, returns decimal"""
     while True:
         user_input = input(promt)
         try:
@@ -13,7 +13,7 @@ def check_for_decimal(promt):
 
 
 def check_for_int(promt):
-    """Validates user input is a whole number"""
+    """Validates user input is a whole number, returns int"""
     while True:
         user_input = input(promt)
         try:
@@ -27,7 +27,7 @@ def check_for_int(promt):
     
 
 def load_settings():
-    """Loads expense tracker settings"""
+    """Loads and returns expense tracker settings"""
     with open("settings.json", "r") as f:
         expense_settings = json.load(f)
         return(expense_settings)
@@ -37,15 +37,26 @@ def get_categories():
     """Returns expense categories"""
     expense_settings = load_settings()
     return expense_settings["categories"]
-    
 
+def validate_category_selection():
+    """Validates user input is a number in the category list, returns category selection (int)"""
+    expense_categories = get_categories()
+    num_of_categories = (len(expense_categories))
+
+    while True:
+        user_input = check_for_int("Enter an expense category (number): ")  # uses helper function to validate user input is an int
+        if user_input <= num_of_categories:     # validates user input is not an int outside of the list
+            return user_input
+        else:
+            print("Please enter a valid category number")
+
+    
 def add_expense():
     expense_name = input("Enter expense name: ")
     expense_amount = check_for_decimal("Enter amount ($)")
     get_categories()
-    expense_category = check_for_int("Enter an expense category (number): ")
+    expense_category = validate_category_selection()
     # need to complete function, still needs to return expense json string, and validate proper category
-
 
 
 
